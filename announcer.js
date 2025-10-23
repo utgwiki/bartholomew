67a2b9e383a10a8abd188e1533f2b7c50f1aef7e
stackd <144233917+toosplatched@users.noreply.github.com>
2025-10-23 19:25:54 +0800

    Set interval to 3 mins

--- announcer.js
+++ announcer.js
@@ -164,7 +164,7 @@ client.once("ready", () => {
     // Set up the interval for continuous checking (every 5 minutes)
     setInterval(() => {
         games.forEach(checkMilestone);
-    }, 300000); 
+    }, 180000); 
 });
 
 client.login(TOKEN);
